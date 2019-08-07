fb7809ddebca4993011f653bceab13f3db1c8598
cally91 <cally91@naver.com>
2019-08-07 11:29:52 +0900

    네이버연습

---
+++ src/main/java/com/biz/naver/config/NaverSecret_Sample.java
@@ -0,0 +1,8 @@
+package com.biz.naver.config;
+
+public class NaverSecret_Sample {
+
+	public final static String NaverClientId="g2115saHEm0KooLMuvau";
+	public final static String NaverClientSecret="gPWfNd6eSD";
+	
+}
